85cd8bb90821de199ea6f7ea16f7955216278f21
andriybakalavr <andriydubinyak@gmail.com>
2019-08-12 16:21:54 +0300

    Initial commit.

---
+++ product/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return "%s" % self.name
+
+    class Meta:
+        verbose_name = 'Категорія товару'
+        verbose_name_plural = 'Категорія товарів'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=64, blank=True, null=True, default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    discount = models.IntegerField(default=0)
+    category = models.ForeignKey(ProductCategory, blank=True, null=True, default=None, on_delete=models.PROTECT)
+    short_description = models.TextField(blank=True, null=True, default=None)
+    description = models.TextField(blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s, %s" % (self.price, self.name)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товари'
+
+
+class ProductImage (models.Model):
+
+    product = models.ForeignKey(Product, blank=True, null=True, default=None, on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='product_images/')
+    is_main = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return "%s, %s" % self.id
+
+    class Meta:
+        verbose_name = 'Фотографі'
+        verbose_name_plural = 'Фотографії'
